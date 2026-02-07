537d3b8a854d44c231678850376e2f3d52cf67d5
prajjwalarizon <prajjwal6122@gmail.com>
2026-02-08 01:30:18 +0530

    feat: implement login page with OTP flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { AuthProvider } from './context/AuthContext'
 import { ToastProvider } from './context/ToastContext'
+import { ToastContainer } from './components/ui/Toast'
+import useToast from './hooks/useToast'
 import AppRoutes from './routes'
+
+// App content wrapper to access toast context
+function AppContent() {
+  const { toasts, removeToast } = useToast();
+
+  return (
+    <>
+      <AppRoutes />
+      <ToastContainer toasts={toasts} onClose={removeToast} />
+    </>
+  );
+}
 
 function App() {
   return (
     <AuthProvider>
       <ToastProvider>
-        <AppRoutes />
+        <AppContent />
       </ToastProvider>
     </AuthProvider>
   )
